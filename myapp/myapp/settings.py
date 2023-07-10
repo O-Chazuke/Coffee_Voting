0216d2b3d0cb02da5c34672a99b44927231a9838
Hiruge-1 <135398486+Hiruge-1@users.noreply.github.com>
2023-07-11 08:49:34 +0900

    Update settings.py

--- myapp/myapp/settings.py
+++ myapp/myapp/settings.py
@@ -20,12 +20,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-!p53yqfp1(@9jeb(%@)xpqlramfbqpb&lkk!o4o^r!gzym!^r8'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['']
 
 
 # Application definition
@@ -80,9 +80,9 @@ DATABASES = {
 #       'NAME': BASE_DIR / 'db.sqlite3',
 
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'Coffee_Voting',
-        'USER': 'root',
-        'PASSWORD': 'Dandelion1110',
+        'NAME': '',
+        'USER': '',
+        'PASSWORD': '',
         'HOST': '127.0.0.1',
         'PORT': '3306',
     }
