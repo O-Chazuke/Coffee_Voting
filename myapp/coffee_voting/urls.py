1ca296e507df1c6ebd586d1b9c2a6a1118011f6f
Hiruge-1 <135398486+Hiruge-1@users.noreply.github.com>
2023-07-11 18:28:28 +0900

    Update urls.py

--- myapp/coffee_voting/urls.py
+++ myapp/coffee_voting/urls.py
@@ -8,5 +8,6 @@ urlpatterns = [
     path('<int:pk>/', views.DetailView.as_view(), name='detail'),
     path('create/', views.Coffee_CreateView.as_view(), name='create'),
     path('<int:pk>/update/', views.UpdateView.as_view(), name='update'),
+    path('<int:pk>/delete/', views.DeleteView.as_view(), name='delete'),
     path('<int:pk>/voting/', views.VotingView.as_view(), name='voting'),
 ]
